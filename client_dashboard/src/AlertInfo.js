84b8107219748a67938e9923ac9c1489a4240bff
zhiqisim <zhiqisim@gmail.com>
2020-03-05 17:04:01 -0800

    update alertinfo

--- client_dashboard/src/AlertInfo.js
+++ client_dashboard/src/AlertInfo.js
@@ -8,18 +8,20 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Title from './Title';
 
+import firebase from './config/firebase';
+
 // Generate Order Data
 function createData(row, date, time, id, location, type) {
   return { row, date, time, id, location, type };
 }
 
-const rows = [
-  createData(0, '16 Mar, 2019', '16:00:24', '75849603', 'Tupelo, MS', 'Fire'),
-  createData(1, '16 Mar, 2019', '15:56:09', '85937284', 'London, UK', 'Chemical Spill'),
-  createData(2, '16 Mar, 2019', '13:44:59', '19205847', 'Boston, MA', 'Fire'),
-  createData(3, '16 Mar, 2019', '10:20:10', '49503854', 'Gary, IN', 'Medical Emergency'),
-  createData(4, '16 Mar, 2019', '08:46:36', '34845933', 'Long Branch, NJ', 'Medical Emergency'),
-];
+// const rows = [
+//   createData(0, '16 Mar, 2019', '16:00:24', '75849603', 'Tupelo, MS', 'Fire'),
+//   createData(1, '16 Mar, 2019', '15:56:09', '85937284', 'London, UK', 'Chemical Spill'),
+//   createData(2, '16 Mar, 2019', '13:44:59', '19205847', 'Boston, MA', 'Fire'),
+//   createData(3, '16 Mar, 2019', '10:20:10', '49503854', 'Gary, IN', 'Medical Emergency'),
+//   createData(4, '16 Mar, 2019', '08:46:36', '34845933', 'Long Branch, NJ', 'Medical Emergency'),
+// ];
 
 function preventDefault(event) {
   event.preventDefault();
@@ -47,7 +49,6 @@ export default function Orders() {
   // Use firestore to listen for changes within
   // our newly created collection
   const listenForAlerts = () => {
-      const unixTime = new Date().getUnixTime();
       firebase.firestore().collection('alarms')
           .onSnapshot((snapshot) => {
               // Loop through the snapshot and collect
@@ -60,6 +61,14 @@ export default function Orders() {
               setAlerts(allAlerts);
           }, (error) => console.error(error));
   };
+
+  if (!alerts) {
+    return (
+        <div>
+            Loading...
+      </div>
+    )
+}
   return (
     <React.Fragment>
       <Title>Current Alerts</Title>
