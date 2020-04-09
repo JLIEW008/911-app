0d273adfa917f883adef95b69f5aa4e93c177340
Jordan <jliew008@e.ntu.edu.sg>
2020-04-08 21:00:16 -0700

    add hard routes

--- client_dashboard/src/App.js
+++ client_dashboard/src/App.js
@@ -2,10 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './Dashboard.js';
+import IOTDevices from './IOTDevices.js';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Dashboard/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={Dashboard} />
+        <Route path="/iot-devices" component={IOTDevices} />
+      </Switch>
+    </BrowserRouter>
+
   );
 }
 
--- client_dashboard/src/listItems.js
+++ client_dashboard/src/listItems.js
@@ -1,40 +1,44 @@
 import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom'
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListSubheader from '@material-ui/core/ListSubheader';
 import DashboardIcon from '@material-ui/icons/Dashboard';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 
+import { config } from './config/firebase';
+
 export const mainListItems = (
   <div>
-    <ListItem button component="a" href="/">
+    <BrowserRouter>
+    <ListItem button component="a" href={config['mainLink']}>
       <ListItemIcon>
         <DashboardIcon />
       </ListItemIcon>
       <ListItemText primary="Dashboard" />
     </ListItem>
-    <ListItem button component="a" href="/">
+    </BrowserRouter>
+    <ListItem button component="a" href={config['mainLink'] + "iot-devices"}>
+      <ListItemIcon>
+        <LayersIcon />
+      </ListItemIcon>
+      <ListItemText primary="IOT Devices" />
+    </ListItem>
+    <ListItem button>
       <ListItemIcon>
         <PeopleIcon />
       </ListItemIcon>
       <ListItemText primary="Manpower" />
     </ListItem>
-    <ListItem button component="a" href="/">
+    <ListItem button>
       <ListItemIcon>
         <BarChartIcon />
       </ListItemIcon>
       <ListItemText primary="Statistics" />
-    </ListItem>
-    <ListItem button component="a" href="/">
-      <ListItemIcon>
-        <LayersIcon />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
     </ListItem>
   </div>
 );
